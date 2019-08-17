90b1c58028dbe2b11d59a4448919669ebe6dee91
Joao Victor Farias <victorfarias.new@gmail.com>
2019-08-17 02:14:53 -0300

    Finalizado

--- FirstActivity/Basic-1.sh
+++ FirstActivity/Basic-1.sh
@@ -15,9 +15,18 @@ while read line; do
 done < professores_formatted.txt
 
 
+# Substituir pelo caminho do projeto nessa parte
+var=1;
 cat conquistas.txt | tr "/" " " | tr " " "_" | tr '[:upper:]' '[:lower:]' > conquistas_formatted.txt;
 while read line; do
     mkdir ./ufc_quixada/redes_de_computadores/conquistas/"$line"
     ln -s -t ~/Documents/Scripts/FirstActivity/ufc_quixada/redes_de_computadores/conquistas/"$line" ~/Documents/Scripts/FirstActivity/ufc_quixada/redes_de_computadores/grade_curricular/"$line"
     mv ~/Documents/Scripts/FirstActivity/ufc_quixada/redes_de_computadores/conquistas/"$line"/"$line" ~/Documents/Scripts/FirstActivity/ufc_quixada/redes_de_computadores/conquistas/"$line"/programa
+    
+    links=$(head -n $var professores_links.txt | tail -n -1 | cat)
+    ln -s -t ~/Documents/Scripts/FirstActivity/ufc_quixada/redes_de_computadores/conquistas/"$line" ~/Documents/Scripts/FirstActivity/ufc_quixada/redes_de_computadores/professores/"$links"/
+    mv ~/Documents/Scripts/FirstActivity/ufc_quixada/redes_de_computadores/conquistas/"$line"/"$links" ~/Documents/Scripts/FirstActivity/ufc_quixada/redes_de_computadores/conquistas/"$line"/professor
+    var=$((var+1))
 done < conquistas_formatted.txt
+
+tar -czvf ufc_quixada.tar.gz ufc_quixada
