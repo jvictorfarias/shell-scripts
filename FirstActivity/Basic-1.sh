4b359e6158c8375e652f9b58857004d53293eccb
Joao Victor Farias <victorfarias.new@gmail.com>
2019-08-17 00:03:46 -0300

    Primeiro Commit

--- FirstActivity/Basic-1.sh
+++ FirstActivity/Basic-1.sh
@@ -1,7 +1,20 @@
 #!/bin/sh
 
+mkdir -p ./ufc_quixada/redes_de_computadores/grade_curricular;
 mkdir -p ./ufc_quixada/redes_de_computadores/professores;
-mkdir -p ufc_quixada/redes_de_computadores/grade_curricular;
+mkdir -p ./ufc_quixada/redes_de_computadores/conquistas;
+
+cat disciplinas.txt | tr "/" " " | tr " " "_" | tr '[:upper:]' '[:lower:]' > disciplinas_formatted.txt;
 while read line; do
     mkdir ./ufc_quixada/redes_de_computadores/grade_curricular/"$line"
-done < disciplinas.txt
+done < disciplinas_formatted.txt
+
+cat professores.txt | tr "/" " " | tr " " "_" | tr '[:upper:]' '[:lower:]' > professores_formatted.txt;
+while read line; do
+    mkdir ./ufc_quixada/redes_de_computadores/professores/"$line"
+done < professores_formatted.txt
+
+cat conquistas.txt | tr "/" " " | tr " " "_" | tr '[:upper:]' '[:lower:]' > conquistas_formatted.txt;
+while read line; do
+    mkdir ./ufc_quixada/redes_de_computadores/conquistas/"$line"
+done < conquistas_formatted.txt
