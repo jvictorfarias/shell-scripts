dfa704a945238c1115e7edff9278a05fc76bca03
Joao Victor Farias <victorfarias.new@gmail.com>
2019-08-21 18:31:35 -0300

    finished: runs in any environment

--- FirstActivity/Basic-1.sh
+++ FirstActivity/Basic-1.sh
@@ -1,31 +1,30 @@
-#!/bin/sh
+#!/bin/bash
 
-mkdir -p ./ufc_quixada/redes_de_computadores/grade_curricular;
-mkdir -p ./ufc_quixada/redes_de_computadores/professores;
-mkdir -p ./ufc_quixada/redes_de_computadores/conquistas;
+# Vars
+var=1;
+pathing=$PWD;
 
 cat disciplinas.txt | tr "/" " " | tr " " "_" | tr '[:upper:]' '[:lower:]' > disciplinas_formatted.txt;
 while read line; do
-    mkdir ./ufc_quixada/redes_de_computadores/grade_curricular/"$line"
+    mkdir -p ./ufc_quixada/redes_de_computadores/grade_curricular/"$line"
 done < disciplinas_formatted.txt
 
 cat professores.txt | tr "/" " " | tr " " "_" | tr '[:upper:]' '[:lower:]' > professores_formatted.txt;
 while read line; do
-    mkdir ./ufc_quixada/redes_de_computadores/professores/"$line"
+    mkdir -p ./ufc_quixada/redes_de_computadores/professores/"$line"
 done < professores_formatted.txt
 
 
-# Substituir pelo caminho do projeto nessa parte
-var=1;
 cat conquistas.txt | tr "/" " " | tr " " "_" | tr '[:upper:]' '[:lower:]' > conquistas_formatted.txt;
 while read line; do
-    mkdir ./ufc_quixada/redes_de_computadores/conquistas/"$line"
-    ln -s -t ~/Documents/Scripts/FirstActivity/ufc_quixada/redes_de_computadores/conquistas/"$line" ~/Documents/Scripts/FirstActivity/ufc_quixada/redes_de_computadores/grade_curricular/"$line"
-    mv ~/Documents/Scripts/FirstActivity/ufc_quixada/redes_de_computadores/conquistas/"$line"/"$line" ~/Documents/Scripts/FirstActivity/ufc_quixada/redes_de_computadores/conquistas/"$line"/programa
-    
-    links=$(head -n $var professores_links.txt | tail -n -1 | cat)
-    ln -s -t ~/Documents/Scripts/FirstActivity/ufc_quixada/redes_de_computadores/conquistas/"$line" ~/Documents/Scripts/FirstActivity/ufc_quixada/redes_de_computadores/professores/"$links"/
-    mv ~/Documents/Scripts/FirstActivity/ufc_quixada/redes_de_computadores/conquistas/"$line"/"$links" ~/Documents/Scripts/FirstActivity/ufc_quixada/redes_de_computadores/conquistas/"$line"/professor
+    mkdir -p ./ufc_quixada/redes_de_computadores/conquistas/"$line";
+    cd $pathing/ufc_quixada/redes_de_computadores/conquistas/"$line";
+    ln -s $pathing/ufc_quixada/redes_de_computadores/grade_curricular/"$line" "$line";
+    cd -;
+    links=$(head -n $var professores_links.txt | tail -n -1 | cat);
+    cd $pathing/ufc_quixada/redes_de_computadores/conquistas/"$line"
+    ln -s $pathing/ufc_quixada/redes_de_computadores/professores/"$links"/ "$links";
+    cd -;
     var=$((var+1))
 done < conquistas_formatted.txt
 
