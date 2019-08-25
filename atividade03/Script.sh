afbb13cdba717738cb491879a84d9259c97a09d2
Joao Victor Farias <victorfarias.new@gmail.com>
2019-08-25 13:38:38 -0300

    finished: atividade finalizada

--- atividade03/Script.sh
+++ atividade03/Script.sh
@@ -4,15 +4,11 @@ key="~/Documents/Scripts/Permission/joaovictor.pem";
 user="joaovictor";
 
 ssh -t -i $key $user@scripts.joao.marcelo.nom.br 'mkdir -p ~/atividades/atividade03';
-ssh -t -i $key $user@scripts.joao.marcelo.nom.br '
-touch ~/atividades/atividade03/emailsordenados.txt;
-touch ~/atividades/atividade03/expressoesregulares.txt;';
 
+scp -i $key $PWD/emailsordenados.txt $user@scripts.joao.marcelo.nom.br:~/atividades/atividade03/;
+scp -i $key $PWD/expressoesregulares.txt $user@scripts.joao.marcelo.nom.br:~/atividades/atividade03/;
 
 scp -i $key $PWD/parte_01.txt $user@scripts.joao.marcelo.nom.br:~/atividades/atividade03/;
 scp -i $key $PWD/parte_02.txt $user@scripts.joao.marcelo.nom.br:~/atividades/atividade03/;
-
-
-# Dúvidas a serem sanadas:
-# Parte02, Q2: Pode ser qualquer pontuação, ou estritamente o .?
-# Parte02, Q3: FRASE COMEÇANDO
+scp -i $key $PWD/parte_03.txt $user@scripts.joao.marcelo.nom.br:~/atividades/atividade03/;
+scp -i $key $PWD/parte_04.txt $user@scripts.joao.marcelo.nom.br:~/atividades/atividade03/;
