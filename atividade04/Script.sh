b60a83e3fcdcd8d7596b6d218717d61f51d866d1
Joao Victor Farias <victorfarias.new@alu.ufc.br>
2019-09-02 03:29:53 -0300

    finished: atividade04 finalizada.

--- atividade04/Script.sh
+++ atividade04/Script.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
 # Questão 1
-echo "grep -E 'Invalid user .+ from' ./auth.log | wc -l" >> ./auth_analise.txt
+#echo "grep -E 'Invalid user .+ from' ./auth.log | wc -l" >> ./auth_analise.txt
 
 # Questão 2
-echo "grep -E 'Invalid user .+ from' ./auth.log > malditos.txt" >> ./auth_analise.txt
+#echo "grep -E 'Invalid user .+ from' ./auth.log > malditos.txt" >> ./auth_analise.txt
 
 # Questão 3
-echo "grep -E 'Accepted publickey for joaovictor' ./auth.log| wc -l" >> ./auth_analise.txt
+#echo "grep -E 'Accepted publickey for joaovictor' ./auth.log| wc -l" >> ./auth_analise.txt
 
 # Upload para o servidor
 
-ssh -t -i '../Permission/joaovictor.pem' joaovictor@scripts.joao.marcelo.nom.br 'mkdir -p ~/atividades/atividade04'
-scp -i '../Permission/joaovictor.pem' ./auth.log joaovictor@scripts.joao.marcelo.nom.br:~/atividades/atividade04/
-scp -i '../Permission/joaovictor.pem' ./malditos.txt joaovictor@scripts.joao.marcelo.nom.br:~/atividades/atividade04/
-scp -i '../Permission/joaovictor.pem' ./auth_analise.txt joaovictor@scripts.joao.marcelo.nom.br:~/atividades/atividade04/
+#ssh -t -i '../Permission/joaovictor.pem' joaovictor@scripts.joao.marcelo.nom.br 'mkdir -p ~/atividades/atividade04'
+scp -i '../Permission/joaovictor.pem' ./despesas.txt joaovictor@scripts.joao.marcelo.nom.br:~/atividades/atividade04/
+scp -i '../Permission/joaovictor.pem' ./atividade04.py joaovictor@scripts.joao.marcelo.nom.br:~/atividades/atividade04/
+scp -i '../Permission/joaovictor.pem' ./parte_01.txt joaovictor@scripts.joao.marcelo.nom.br:~/atividades/atividade04/
+scp -i '../Permission/joaovictor.pem' ./parte_02.txt joaovictor@scripts.joao.marcelo.nom.br:~/atividades/atividade04/
 
---
+++ atividade04/atividade04.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+import time
+NOTA1 = 7.0
+NOTA2 = 8.0
+NOTAFINAL = (NOTA1 + NOTA2) / 2
+print(NOTAFINAL)
+print(time.ctime())
