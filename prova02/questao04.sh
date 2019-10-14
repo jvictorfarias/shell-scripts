22834dd446f8a691d6abc1383332eafca8c71f7a
Joao Victor Farias <victorfarias.new@alu.ufc.br>
2019-10-14 00:07:57 -0300

    prova02: q4 details

--- prova02/questao04.sh
+++ prova02/questao04.sh
@@ -5,35 +5,33 @@ multTable(){
     tput setaf 4
     tput bold
     tput cup 0 3
-    seq -s "  " 1 $1
+    seq -s "   " 1 $1
     
-    tput cup 2 0
-    for ((i=2; i <= `expr $1 + 1`; i++))
+    tput cup 1 0
+    for ((i=1; i <= $1; i++))
     do
         tput cup $i 0
-        printf "%d" `expr $i - 1`
+        printf "%d" $i
         
     done
     
     tput sgr0
     COLUMN_HANDLER=3
-    for ((j=2; j <= `expr $1 + 1`; j++))
+    for ((j=1; j <= $1; j++))
     do
-        for ((k=2; k <= `expr $1 + 1`; k++))
+        for ((k=1; k <= $1; k++))
         do
             tput cup $k $COLUMN_HANDLER
-            VAL=`expr $k - 1`
-            MULT=`expr $j - 1`
-            if [ $VAL -eq $MULT ]
+            if [ $k -eq $j ]
             then
                 tput smul
-                echo "`expr $VAL \* $MULT`"
+                echo "`expr $k \* $j`"
                 tput rmul
             else
-                echo "`expr $VAL \* $MULT`"
+                echo "`expr $k \* $j`"
             fi
         done
-        COLUMN_HANDLER=`expr $COLUMN_HANDLER + 3`
+        COLUMN_HANDLER=`expr $COLUMN_HANDLER + 4`
     done
     
     
